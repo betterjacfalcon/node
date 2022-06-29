91b39176d7b35b0dd9bf304ae3dd373926f63cd9
betterjacfalcon <jacfalcon.better.practices@gmail.com>
2022-06-29 17:32:43 -0600

    Inicio de Section_4 Base de Node

---
+++ section_4/app.js
@@ -0,0 +1,8 @@
+console.clear();
+console.log('==========================')
+console.log('       Tabla del 5')
+console.log('==========================')
+const valor = 5;
+for (let i=1;i<=10;i++) { 
+   console.log(`${i} *  ${valor} = ${valor * i} `);
+}
