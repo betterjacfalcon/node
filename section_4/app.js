08432b043848084b41b4239568d4d06e676028e8
betterjacfalcon <jacfalcon.better.practices@gmail.com>
2022-06-29 19:36:21 -0600

    Generacion de file.txt utilizando el file system

--- section_4/app.js
+++ section_4/app.js
@@ -1,8 +1,18 @@
+const fs = require('fs');
+
 console.clear();
 console.log('==========================')
 console.log('       Tabla del 5')
 console.log('==========================')
-const valor = 5;
+const valor = 4;
+let salida = '';
 for (let i=1;i<=10;i++) { 
-   console.log(`${i} *  ${valor} = ${valor * i} `);
+   salida += ` ${i} *  ${valor} = ${valor * i} \n`;
 }
+
+console.log(salida);
+
+fs.writeFile(`tabla-${valor}.txt`, salida, (err) => {
+    if (err) throw err;
+    console.log('archivo creado');
+})
