8fdf39d7fc1fb998163d3bbbd85e691ffca9c362
betterjacfalcon <jacfalcon.better.practices@gmail.com>
2022-07-04 05:07:22 -0600

    Recibir informacion desde la linea de comando - Argumento 3

--- section_4/app.js
+++ section_4/app.js
@@ -1,7 +1,14 @@
 const { crearArhivo } = require('./helpers/multiplicar');
 
-const valor = 4;
+
 console.clear();
+
+const [ , , arg3='valor=5'] = process.argv;
+const [ , valor=5] = arg3.split('=');
+console.log(valor);
+
+//const valor = 4;
+
 crearArhivo(valor)
     .then( nombreArchivo => console.log(nombreArchivo,'creado') )
     .catch(err => console.log(err));
