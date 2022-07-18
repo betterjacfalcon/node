748b6189cacb172e6e637d82cd777e1556070d7e
betterjacfalcon <jacfalcon.better.practices@gmail.com>
2022-07-18 05:48:05 -0600

    Repetir el menu hasta seleccionar la opcion salir

--- section_5/helpers/mensaje.js
+++ section_5/helpers/mensaje.js
@@ -1,43 +1,54 @@
+const { resolve } = require('path');
+
 require('colors');
 
 const mostrarMenu = () => {
-    console.clear();
-    console.log('==============================='.yellow);
-    console.log('Seleccione una opcion'.yellow);
-    console.log('==============================='.yellow);
-    console.log(`${'1.'.yellow} Crear una tarea`);
-    console.log(`${'2.'.yellow}  Listar las tarea`);
-    console.log(`${'3.'.yellow}  Listar las tarea completadas`);
-    console.log(`${'4.'.yellow}  Listar las tarea pendientes`);
-    console.log(`${'5.'.yellow}  Completar tarea(s)`);
-    console.log(`${'6.'.yellow}  Borrar tarea`);
-    console.log(`${'0.'.yellow}  Salir \n`);
 
-    const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
+    return new Promise((resolve) => {
+        console.clear();
+        console.log('==============================='.yellow);
+        console.log('Seleccione una opcion'.yellow);
+        console.log('==============================='.yellow);
+        console.log(`${'1.'.yellow} Crear una tarea`);
+        console.log(`${'2.'.yellow}  Listar las tarea`);
+        console.log(`${'3.'.yellow}  Listar las tarea completadas`);
+        console.log(`${'4.'.yellow}  Listar las tarea pendientes`);
+        console.log(`${'5.'.yellow}  Completar tarea(s)`);
+        console.log(`${'6.'.yellow}  Borrar tarea`);
+        console.log(`${'0.'.yellow}  Salir \n`);
+
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+
+        });
+
+        readline.question('Selecciones una opcion: ', (opt) => {
+            readline.close();
+            resolve(opt);
+        });
 
     });
 
-    readline.question('Selecciones una opcion: ', (opt) => {
-        console.log({ opt });
-        readline.close();
-    });
+
 }
 
 const pausa = () => {
-    const readline = require('readline').createInterface({
-        input: process.stdin,
-        output: process.stdout
-
-    });
-
-    readline.question(`\nPresion ${'ENTER'.blue}\n`, (opt) => {
+    return new Promise((resolve) =>{
+        const readline = require('readline').createInterface({
+            input: process.stdin,
+            output: process.stdout
+    
+        });
+    
+        readline.question(`\nPresion ${'ENTER'.blue}\n`, (opt) => {
             readline.close();
+            resolve();
+        });
     });
 }
 
-module.exports ={
+module.exports = {
     mostrarMenu,
     pausa
 }
